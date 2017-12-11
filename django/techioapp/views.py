3f8af84a8ebfe72498b041e772ba431aacc090f4
Léon <leon.vandebeek@xs4all.nl>
2017-12-12 00:23:55 +0100

    Some posts

--- django/techioapp/views.py
+++ django/techioapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import todo
 
-# Create your views here.
 def post_list(request):
-    return render(request, 'techioapp/post_list.html')
+    posts = todo.objects.filter(completed_date__lte=timezone.now()).order_by('completed_date')
+    return render(request, 'techioapp/post_list.html', {'posts': posts})
