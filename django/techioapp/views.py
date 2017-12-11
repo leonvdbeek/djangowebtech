fc3430b23ad3905778f18a868e339b59c2771f71
Léon <leon.vandebeek@xs4all.nl>
2017-12-12 00:35:34 +0100

    a

--- django/techioapp/views.py
+++ django/techioapp/views.py
@@ -3,5 +3,5 @@ from django.utils import timezone
 from .models import todo
 
 def todo_list(request):
-    todos = todo.objects.filter(completed_date__lte=timezone.now()).order_by('completed_date')
+    todos = todo.objects.order_by('completed_date')
     return render(request, 'techioapp/todo_list.html', {'todos': todos})
