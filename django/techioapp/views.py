af2bf626bdfa6bc1e31886cae55bf85f94b750aa
Léon <leon.vandebeek@xs4all.nl>
2017-12-12 00:31:21 +0100

    Changed everything to todo

--- django/techioapp/views.py
+++ django/techioapp/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import todo
 
-def post_list(request):
-    posts = todo.objects.filter(completed_date__lte=timezone.now()).order_by('completed_date')
-    return render(request, 'techioapp/post_list.html', {'posts': posts})
+def todo_list(request):
+    todos = todo.objects.filter(completed_date__lte=timezone.now()).order_by('completed_date')
+    return render(request, 'techioapp/todo_list.html', {'todos': todos})
