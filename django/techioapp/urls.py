79f206215a8fd84222edb0053c435323d3fe83c6
Léon <leon.vandebeek@xs4all.nl>
2017-12-17 17:39:46 +0100

    Added a form

--- django/techioapp/urls.py
+++ django/techioapp/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.todo_list, name='todo_list'),
     url(r'^todo/(?P<pk>\d+)/$', views.todo_detail, name='todo_detail'),
+    url(r'^todo/new/$', views.todo_new, name='todo_new'),
+    url(r'^howdoesitwork$', views.todo_new, name='howdoesitwork'),
+    url(r'^shoppingcart$', views.todo_new, name='shoppingcart'),
+    url(r'^index$', views.todo_new, name='index'),
 ]
--- django/techioapp/views.py
+++ django/techioapp/views.py
@@ -9,3 +9,16 @@ def todo_list(request):
 def todo_detail(request, pk):
     todo = get_object_or_404(Todo, pk=pk)
     return render(request, 'techioapp/todo_detail.html', {'todo': todo})
+
+def todo_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            todo = form.save(commit=False)
+            todo.author = request.user
+            todo.deadline_date = timezone.now()
+            todo.save()
+            return redirect('post_detail', pk=todo.pk)
+    else:
+        form = PostForm()
+    return render(request, 'techioapp/todo_edit.html', {'form': form})
