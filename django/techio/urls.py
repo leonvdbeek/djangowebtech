98c73deb945ebf3c6765c8bff64cbb1c99a32d5d
Léon <leon.vandebeek@xs4all.nl>
2017-12-17 18:37:05 +0100

    trying to fix the links

--- django/techio/urls.py
+++ django/techio/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('techioapp.urls'))
+    url(r'^', include('techioapp.urls', namespace="techioapp")),
 ]
