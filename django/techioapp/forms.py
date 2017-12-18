5d77f2a3142dea4068add4c940562fe963b33ac0
leonvdbeek <leon.vandebeek@xs4all.nl>
2017-12-18 12:10:39 +0100

    added item to admin

--- django/techioapp/forms.py
+++ django/techioapp/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 
-from .models import Todo
+from .models import Todo, Item
 
 class Todoform(forms.ModelForm):
 
     class Meta:
         model = Todo
         fields = ('title', 'text',)
+
+class Itemform(forms.ModelForm):
+
+    class Meta:
+        model = Item
+        fields = ('name', 'info', 'price', 'stock', 'upload')
--- django/techioapp/views.py
+++ django/techioapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-from .models import Todo
+from .models import Todo, Item
 from django.shortcuts import redirect
 from .forms import Todoform
 
