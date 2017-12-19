45e65f8c12db56474449768ce46b23efc72052e7
Léon <leon.vandebeek@xs4all.nl>
2017-12-19 10:12:23 +0100

    Trying path setting of imagefield to the pk

--- django/techioapp/models.py
+++ django/techioapp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.encoding import force_unicode
+import os.path
 
 class Todo(models.Model):
     author = models.ForeignKey('auth.User')
@@ -17,16 +19,53 @@ class Todo(models.Model):
     def __str__(self):
         return self.title
 
-class Item(models.Model):
-    name = models.CharField(max_length=20)
-    info = models.TextField()
-    price = models.DecimalField(max_digits=5, decimal_places=2)
-    stock = models.IntegerField()
-    upload = models.FileField(upload_to='djangowebtech/django/techioapp/static/img')
 
-    def bought(self):
-        self.stock = self.stock-1
-        self.save()
 
-    def __str__(self):
-        return self.name
+class RenameFilesModel(models.Model):
+
+    class Item(RenameFilesModel):
+        name = models.CharField(max_length=20)
+        info = models.TextField()
+        price = models.DecimalField(max_digits=5, decimal_places=2)
+        stock = models.IntegerField()
+        file = models.ImageField(upload_to='djangowebtech/django/techioapp/static/img')
+
+        def bought(self):
+            self.stock = self.stock-1
+            self.save()
+    
+        def __str__(self):
+            return self.name
+
+    RENAME_FILES = {
+        'file': {'dest': 'uploads/photos', 'keep_ext': True}
+    }
+
+    class Meta:
+        abstract = True
+
+    def save(self, force_insert=False, force_update=False):
+        rename_files = getattr(self, 'RENAME_FILES', None)
+        if rename_files:
+            super(RenameFilesModel, self).save(force_insert, force_update)
+            force_insert, force_update = False, True
+
+            for field_name, options in rename_files.iteritems():
+                field = getattr(self, field_name)
+                file_name = force_unicode(field)
+                name, ext = os.path.splitext(file_name)
+                keep_ext = options.get('keep_ext', True)
+                final_dest = options['dest']
+                if callable(final_dest):
+                    final_name = final_dest(self, file_name)
+                else:
+                    final_name = os.path.join(final_dest, '%s' % (self.pk,))
+                    if keep_ext:
+                        final_name += ext
+                if file_name != final_name:
+                    field.storage.delete(final_name)
+                    field.storage.save(final_name, field)
+                    field.storage.delete(file_name)
+                    setattr(self, field_name, final_name)
+
+        super(RenameFilesModel, self).save(force_insert, force_update)
