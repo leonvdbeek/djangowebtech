e1e51b0c492bc9d5e54a8494c1f0da5b4d3c7d9a
Léon <leon.vandebeek@xs4all.nl>
2017-12-19 12:12:48 +0100

    aa

--- django/techioapp/views.py
+++ django/techioapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Todo, Item
 from django.shortcuts import redirect
-from .forms import Todoform
+from .forms import Todoform, Itemform
 
 
 
