dcd381da68282a8abea18e9e472e59a5603f4640
Léon <leon.vandebeek@xs4all.nl>
2017-12-19 14:06:05 +0100

    try

--- django/techioapp/urls.py
+++ django/techioapp/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.item_list, name='item_list'),
     url(r'^item/(?P<pk>\d+)/$', views.item_detail, name='item_detail'),
-    url(r'^item/new/$', views.item_new, name='item_new'),
+    url(r'^item/new/$', views.simple_upload, name='simple_upload'),
     url(r'^item/(?P<pk>\d+)/edit/$', views.item_edit, name='item_edit'),
 ]
