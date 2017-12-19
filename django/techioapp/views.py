2f53ed0085cce229974a7c258e2c24884ef1e063
Léon <leon.vandebeek@xs4all.nl>
2017-12-19 11:58:25 +0100

    New stuffs

--- django/techioapp/views.py
+++ django/techioapp/views.py
@@ -10,6 +10,40 @@ def item_list(request):
     items = Item.objects.all()
     return render(request, 'techioapp/item_list.html', {'items': items})
 
+def item_new(request):
+    if request.method == "POST":
+        form = Itemform(request.POST)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.save()
+            return redirect('item_detail', pk=item.pk)
+    else:
+        form = Itemform()
+    return render(request, 'techioapp/item_edit.html', {'form': form})
+
+def item_detail(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    return render(request, 'techioapp/item_detail.html', {'item': item})
+
+def item_edit(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    if request.method == "POST":
+        form = Itemform(request.POST, instance=item)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.save()
+            return redirect('item_detail', pk=item.pk)
+    else:
+        form = Itemform(instance=item)
+    return render(request, 'techioapp/item_edit.html', {'form': form})
+
+
+
+
+
+
+
+
 
 def todo_detail(request, pk):
     todo = get_object_or_404(Todo, pk=pk)
