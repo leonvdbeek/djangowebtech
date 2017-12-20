208351f54df0eb3c543319749832815166e1db3f
Léon <leon.vandebeek@xs4all.nl>
2017-12-21 00:27:51 +0100

    a

--- django/techioapp/views.py
+++ django/techioapp/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import redirect
 from .forms import Itemform
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import ItemSerializer
