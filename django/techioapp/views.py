52b5ab13f89cf039eb83a1db7050b8b37cef38ce
Léon <leon.vandebeek@xs4all.nl>
2017-12-21 13:51:23 +0100

    added post and delete to rest api

--- django/techioapp/views.py
+++ django/techioapp/views.py
@@ -11,22 +11,36 @@ from rest_framework import status
 from .serializers import ItemSerializer
 
 class ItemList(APIView):
-    def get(self, request):
+    def get(self, request, format=None):
         items = Item.objects.all()
         serializer = ItemSerializer(items, many=True)
         return Response(serializer.data)
 
-    def post(self):
-        pass
+    def post(self, request, format=None):
+        serializer = ItemSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response (serializer.data, status=HTTP_201_CREATED)
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
 
 class ItemListElement(APIView):
-    def get(self, request, pk):
+    def get(self, request, pk, format=None):
         item = Item.objects.get(pk=pk)
         serializer = ItemSerializer(item)
         return Response(serializer.data)
 
-    def post(self):
-        pass
+    def post(self, request, pk, format=None):
+        item = Item.objects.get(pk=pk)
+        serializer = ItemSerializer(item, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        item = Item.objects.get(pk=pk)
+        item.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 def item_list(request):
     items = Item.objects.all()
